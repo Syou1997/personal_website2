6523f9dff1cf353f43226b80bdd6cb3fb6dd2888
Syou Zhang <azwsxrfv2396187@gmail.com>
2025-09-21 16:36:15 +0900

    PW

--- src/main.js
+++ src/main.js
@@ -13,14 +13,14 @@ app.use(router)
 app.mount('#app')
 
 //  // 設定密碼
-//     const correctPassword = "TaiwanSyou";
+    const correctPassword = "TaiwanSyou";
 
-//     // 顯示密碼提示
-//     const input = prompt("請輸入密碼進入本頁面：");
+    // 顯示密碼提示
+    const input = prompt("請輸入密碼進入本頁面：");
 
-//     if (input !== correctPassword) {
-//       // 密碼錯誤時跳轉到其他頁面或顯示錯誤
-//       alert("密碼錯誤，將返回首頁");
-//       window.location.href = "https://google.com"; // 或任何你希望導向的頁面
-//     }
+    if (input !== correctPassword) {
+      // 密碼錯誤時跳轉到其他頁面或顯示錯誤
+      alert("密碼錯誤，將返回首頁");
+      window.location.href = "https://google.com"; // 或任何你希望導向的頁面
+    }
 
